01cfe90e8f7afa9169714b030a2ffb001a62289f
591285327 <18293747380@163.com>
2019-06-10 18:00:58 +0800

    add Constants class

--- src/main/java/cn/vip/controller/LoginController.java
+++ src/main/java/cn/vip/controller/LoginController.java
@@ -3,35 +3,45 @@ package cn.vip.controller;
 import cn.vip.pojo.AuUser;
 import cn.vip.service.AuUserService;
 import cn.vip.utils.Constants;
+import cn.vip.utils.JacksonUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 
 @Controller
+@RequestMapping
 public class LoginController {
 
     @Resource
     private AuUserService auUserService;
 
+    /**
+     * 用户登陆
+     * @param user
+     * @param session
+     * @return
+     * @throws IOException
+     */
     @RequestMapping(value = "/login.html", method = RequestMethod.POST)
     @ResponseBody
-    public String liginDo(String logincode, String password, HttpSession session){
-        AuUser auUser = null;
-        auUser=auUserService.loginDo(logincode);
+    public String liginDo(@RequestParam(value ="user") String user, HttpSession session) throws IOException {
+        AuUser loginUser = JacksonUtil.json2Bean(user, AuUser.class);
+
+        AuUser auUser=auUserService.loginDo(loginUser.getLogincode());
         if (auUser != null){
-            if(!auUser.getPassword().equals(password)){
-                auUser=null;
-                return "failed";
+            if(!auUser.getPassword().equals(loginUser.getPassword())){
+                return "pwderror";
             }else {
                 session.setAttribute("auUser", Constants.LOGIN_USER);
                 return "success";
             }
         }else {
-            auUser=null;
             return "nologincode";
         }
     }

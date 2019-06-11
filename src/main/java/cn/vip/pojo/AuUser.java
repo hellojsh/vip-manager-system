dd1af63e54eb16c227fccf2ea0a5f00f8d79cacc
hellojsh_glb <hellojsh_glb@outlook.com>
2019-06-11 16:23:49 +0800

    modify AuUser.java add Annotation JsonFormat,DateTimeFormat

--- src/main/java/cn/vip/pojo/AuUser.java
+++ src/main/java/cn/vip/pojo/AuUser.java
@@ -1,5 +1,8 @@
 package cn.vip.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.util.Date;
 
 public class AuUser {
@@ -33,6 +36,8 @@ public class AuUser {
 
     private String postCode;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date createTime;
 
     private Long referId;
@@ -49,8 +54,12 @@ public class AuUser {
 
     private Integer isStart;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date lastUpdateTime;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date lastLoginTime;
 
     private String bankAccount;
